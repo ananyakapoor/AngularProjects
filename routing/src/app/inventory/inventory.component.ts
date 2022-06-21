d7598200d0797a41149ef8a2bfedf57e61e786de
ananyakapoor <57395900+ananyakapoor@users.noreply.github.com>
2022-06-21 12:24:38 +0530

    Add files via upload

---
+++ routing/src/app/inventory/inventory.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-inventory',
+  templateUrl: './inventory.component.html',
+  styleUrls: ['./inventory.component.css']
+})
+export class InventoryComponent implements OnInit {
+
+  constructor(private router: Router) { }
+  onBlack():void{
+    this.router.navigate(['/product']);
+  }
+  ngOnInit(): void {
+  }
+
+}
